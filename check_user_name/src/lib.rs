f8d8f53f5774d4924a6929586c3f368f5f3e0233
Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-05-19 10:20:12 +0100

    fix it

---
+++ check_user_name/src/lib.rs
@@ -0,0 +1,10 @@
+pub enum AccessLevel {}
+
+pub struct User {}
+
+impl User {
+  pub fn new(name: String, level: AccessLevel) -> User {}
+  pub fn send_name(&self) -> Option<&str> {}
+}
+
+pub fn check_user_name(user: &User) -> (bool, &str) {}
